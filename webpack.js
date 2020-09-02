569eabedea03f5a1db5ef438714d83cd0a42d72c
Julien Veyssier <eneiluj@posteo.net>
2020-09-02 11:22:23 +0200

    0.0.1
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- webpack.js
+++ webpack.js
@@ -3,12 +3,16 @@ const path = require('path')
 const webpack = require('webpack')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
+if (webpackConfig.entry && webpackConfig.entry.main) {
+	delete webpackConfig.entry.main
+}
+
 const config = {
-    entry: {
+	entry: {
 		personalSettings: path.join(__dirname, 'src', 'personalSettings.js'),
 		adminSettings: path.join(__dirname, 'src', 'adminSettings.js'),
 		dashboard: path.join(__dirname, 'src', 'dashboard.js'),
-    },
+	},
 	module: {
 		rules: [
 			{
