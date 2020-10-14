4ffbdf6c25bfd9de7ad97e097d2e429c8a07a9f0
Julien Veyssier <eneiluj@posteo.net>
2020-10-14 17:48:53 +0200

    get rid of the compilation error
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/components/AdminSettings.vue
+++ src/components/AdminSettings.vue
@@ -119,29 +119,36 @@ export default {
 .grid-form label {
 	line-height: 38px;
 }
+
 .grid-form input {
 	width: 100%;
 }
+
 .grid-form {
 	max-width: 500px;
 	display: grid;
 	grid-template: 1fr / 1fr 1fr;
 	margin-left: 30px;
 }
+
 #gitlab_prefs .icon {
 	display: inline-block;
 	width: 32px;
 }
+
 #gitlab_prefs .grid-form .icon {
 	margin-bottom: -3px;
 }
+
 .icon-gitlab {
 	background-image: url(./../../img/app-dark.svg);
 	background-size: 23px 23px;
 	height: 23px;
 	margin-bottom: -4px;
 }
+
 body.theme--dark .icon-gitlab {
 	background-image: url(./../../img/app.svg);
 }
+
 </style>
--- src/components/PersonalSettings.vue
+++ src/components/PersonalSettings.vue
@@ -222,9 +222,11 @@ export default {
 .gitlab-grid-form label {
 	line-height: 38px;
 }
+
 .gitlab-grid-form input {
 	width: 100%;
 }
+
 .gitlab-grid-form {
 	max-width: 600px;
 	display: grid;
@@ -233,26 +235,33 @@ export default {
 		margin-bottom: -1px;
 	}
 }
+
 #gitlab_prefs .icon {
 	display: inline-block;
 	width: 32px;
 }
+
 #gitlab_prefs .grid-form .icon {
 	margin-bottom: -3px;
 }
+
 .icon-gitlab {
 	background-image: url(./../../img/app-dark.svg);
 	background-size: 23px 23px;
 	height: 23px;
 	margin-bottom: -4px;
 }
+
 body.theme--dark .icon-gitlab {
 	background-image: url(./../../img/app.svg);
 }
+
 #gitlab-content {
 	margin-left: 40px;
 }
+
 #gitlab-search-block .icon {
 	width: 22px;
 }
+
 </style>
