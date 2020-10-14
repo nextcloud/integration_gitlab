0c6b514cee59e23d036618c753276392673570d5
Julien Veyssier <eneiluj@posteo.net>
2020-10-14 19:05:36 +0200

    bump to webpack 5
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- webpack.js
+++ webpack.js
@@ -1,10 +1,14 @@
 const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
+const buildMode = process.env.NODE_ENV
+const isDev = buildMode === 'development'
+webpackConfig.devtool = isDev ? 'cheap-source-map' : 'source-map'
+
 webpackConfig.entry = {
-	personalSettings: path.join(__dirname, 'src', 'personalSettings.js'),
-	adminSettings: path.join(__dirname, 'src', 'adminSettings.js'),
-	dashboard: path.join(__dirname, 'src', 'dashboard.js'),
+    personalSettings: { import: path.join(__dirname, 'src', 'personalSettings.js'), filename: 'integration_gitlab-personalSettings.js' },
+    adminSettings: { import: path.join(__dirname, 'src', 'adminSettings.js'), filename: 'integration_gitlab-adminSettings.js' },
+    dashboard: { import: path.join(__dirname, 'src', 'dashboard.js'), filename: 'integration_gitlab-dashboard.js' },
 }
 
 module.exports = webpackConfig
