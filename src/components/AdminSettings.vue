6d52cac68f04e4706745583bffe5fe66d299ebab
Julien Veyssier <eneiluj@posteo.net>
2021-02-08 14:53:39 +0100

    fix dialog style import
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- src/components/AdminSettings.vue
+++ src/components/AdminSettings.vue
@@ -57,6 +57,7 @@ import { generateUrl } from '@nextcloud/router'
 import axios from '@nextcloud/axios'
 import { delay } from '../utils'
 import { showSuccess, showError } from '@nextcloud/dialogs'
+import '@nextcloud/dialogs/styles/toast.scss'
 
 export default {
 	name: 'AdminSettings',
