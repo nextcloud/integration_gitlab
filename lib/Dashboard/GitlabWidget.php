aeeadf02fe7df55de0ce98e7b39fb2e722e6fc45
Valdnet <47037905+Valdnet@users.noreply.github.com>
2022-06-15 14:46:17 +0200

    l10n: Change to capital letters
    Signed-off-by: Valdnet <47037905+Valdnet@users.noreply.github.com>

--- lib/Dashboard/GitlabWidget.php
+++ lib/Dashboard/GitlabWidget.php
@@ -56,7 +56,7 @@ class GitlabWidget implements IWidget {
 	 * @inheritDoc
 	 */
 	public function getTitle(): string {
-		return $this->l10n->t('GitLab to-dos');
+		return $this->l10n->t('GitLab To-Dos');
 		}
 
 	/**
