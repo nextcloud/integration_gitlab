92f965ad5fb10ce432b59531d96dd9c787828ec5
provokateurin <kate@provokateurin.de>
2024-07-15 12:04:33 +0200

    build: Add PSR-4 autoloading to make tests work
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../../../tests/bootstrap.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-\OC_App::loadApps();
+\OC_App::loadApp(OCA\Gitlab\AppInfo\Application::APP_ID);
 OC_Hook::clear();
